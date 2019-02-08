c67a550da386b5c6b4705d6e909086644544f225
Mike Pelletier <mike@mkp.ca>
2019-02-08 13:41:11 -0500

    Run code in my RAM and ROM on `emacs-nes`'s 6502

--- e65.el
+++ e65.el
@@ -31,11 +31,11 @@
 (defun e65-device-write (device addr data)
   (let ((bus (e65-device-bus device)))
     (dolist (target (e65-bus-devices bus))
-      (and (not (= device target))
+      (and (not (eq device target))
            (let ((addr (funcall (e65-device-map-function target)
                                 target addr)))
              (and addr (funcall (e65-device-write-function target)
-                                addr data)))))))
+                                target addr data)))))))
 
 ;; Allow device to read from the bus by delegating to every
 ;; interested device attached to the bus except the reader
@@ -43,11 +43,11 @@
   (let ((bus (e65-device-bus device))
         values)
     (dolist (read-device (e65-bus-devices bus))
-      (and (not (= device read-device))
+      (and (not (eq device read-device))
            (let* ((addr (funcall (e65-device-map-function read-device)
                                  read-device addr))
-                  (data (and addr (funcall (e65-device-read-function
-                                            read-device) addr))))
+                  (data (and addr (funcall (e65-device-read-function read-device)
+                                           read-device addr))))
              (and data (push data values)))))
     (cond
      ((not values) #x0000)              ; Or is it #xffff?
@@ -63,19 +63,33 @@
 ;;; e65-6264 SRAM
 
 (defun e65-6264-write (device addr data)
-  (aset (e65-6264-state device)
-        (logand addr #x01ff)            ; Only 13 address pins
+  (aset (e65-device-state device)
+        (logand addr #x1fff)            ; Only 13 address pins
         (logand data #xff)))
 
 (defun e65-6264-read (device addr)
-  (aref (e65-6264-state device)
-        (logand addr #x01ff)))          ; Only 13 address pins
+  (aref (e65-device-state device)
+        (logand addr #x1fff)))          ; Only 13 address pins
 
 (defun make-e65-6264 (map-function)
-  (make-e65-device :state (make-vector #x01ff #x00)
+  (make-e65-device :state (make-vector #x2000 #x00)
                    :map-function map-function
                    :write-function 'e65-6264-write
                    :read-function 'e65-6264-read))
+
+
+(defun e65-display-6264 (ram)
+  (switch-to-buffer "e65-6264 contents")
+  (delete-region (point-min) (point-max))
+  (let ((data (e65-device-state ram)))
+    (dotimes (i #x2000)
+      (when (= 0 (mod i 16))
+        (insert (format "%04x: " i)))
+      (insert (format "%02x " (aref data i)))
+      (when (= 0 (mod (1+ i) 16))
+        (insert "\n"))
+      (when (= 0 (mod (1+ i) 256))
+        (insert "\n")))))
 
 ;;; e65-rom
 
@@ -92,19 +106,57 @@
                    :read-function (lambda (device addr)
                                     (aref (e65-device-state device) addr))))
 
+;;; e65-6502
+
+(defun make-e65-6502 ()
+  (make-e65-device
+   :state (make-nes/cpu :interrupt (make-nes/interrupt))
+   :signal-function #'e65-6502--signal))
+
+(defun e65-6502--reset (device)
+  (let ((state (e65-device-state device)))
+    ;; Three cycles of suppressed PUSHes
+    ;; ...
+    ;; Copy RESET vector to PC
+    (setf (e65-6502-state-pc state)
+          (logior (e65-device-read device #xfffc)
+                  (lsh (e65-device-read device #xfffd) 8)))))
+
+(defun e65-6502--signal (device signals)
+  (cond
+   ((member :reset signals)
+    (e65-6502--reset device))
+   ((member :phi0 signals)
+    ;; Occlude nes/cpu--bus-read and nes/cpu--bus-write
+    (cl-letf (((symbol-function 'nes/cpu--bus-read)
+               (lambda (C addr) (e65-device-read device addr)))
+              ((symbol-function 'nes/cpu--bus-write)
+               (lambda (C addr data) (e65-device-write device addr data))))
+      (nes/cpu-step (e65-device-state device))))))
+
 ;;; Scratch
 
+(defun e65-bus-tick (bus)
+  (e65-bus-emit bus :phi0))
+
 (defun e65-scratch ()
-  (let ((bus (make-e65-bus)))
+  (let ((bus (make-e65-bus))
+        (ram (make-e65-6264
+              (lambda (device addr)
+                (and (> #x2000 addr) addr)))))
+    ;; Install CPU
+    (e65-add-device bus (make-e65-6502))
+    
     ;; Install 8K RAM at 0x0000-0x1fff
-    (e65-add-device bus (make-e65-6264
-                         (lambda (addr)
-                           (and (> #x2000 addr) addr))))
+    (e65-add-device bus ram)
     ;; Install a ROM in upper 8K
-    (e65-add-device bus (make-e65-rom-from-file
-                         "random"
-                         (lambda (addr)
+    (e65-add-device bus (make-e65-rom
+                         (make-vector #x2000 #x00)
+                         (lambda (device addr)
                            (and (<= #xe000 addr)
                                 (logand #x01ff addr)))))
     ;; Crank it
-    (e65-bus-emit bus :phi0)))
+    (dotimes (i 3) (e65-bus-tick bus))
+    ;; Show the output; note the result of executing three BRK
+    ;; instructions in the stack in page one!
+    (e65-display-6264 ram)))
